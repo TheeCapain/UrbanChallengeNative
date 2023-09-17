573b2e263983f7339251150f7828890baa0b1c98
August <Augusthauerslev@outlook.com>
2023-09-17 12:01:43 +0200

    added a map view for the game screen

--- client/Screens/game.js
+++ client/Screens/game.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button } from 'react-native';
-import { useState } from 'react';
+import { useState } from 'react'
+import MapView from "react-native-maps";
 
-export default function Home({ navigation }) {
+export default function Game({ navigation }) {
+    const [region, setRegion] = useState({
+        latitude: 51.5079145,
+        longitude: -0.0899163,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+    });
+
 
     return (
         <View style={styles.container}>
             <View><Text>This is the game screen</Text></View>
+            <MapView
+                style={styles.map}
+                //specify our coordinates.
+                initialRegion={{
+                    latitude: 55.676098,
+                    longitude: 12.568337,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                //onRegionChangeComplete runs when the user stops dragging MapView
+                onRegionChangeComplete={(region) => setRegion(region)}
+            />
+
+
+            {/*Display user's current region:*/}
+            <Text style={styles.text}>Current latitude: {region.latitude}</Text>
+            <Text style={styles.text}>Current longitude: {region.longitude}</Text>
         </View>
 
     );
@@ -14,9 +39,12 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#313638',
-        alignItems: 'center',
-        justifyContent: 'center',
+        ...StyleSheet.absoluteFillObject,
+        flex: 1, //the container will fill the whole screen.
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
     },
 });
