2768ee7f2e376234dfa824b4994df38f46af82c7
August <Augusthauerslev@outlook.com>
2023-10-13 19:12:50 +0200

    fixed merge conflict

--- client/Screens/game.js
+++ client/Screens/game.js
@@ -10,14 +10,12 @@ import LobbyMenu from '../Screens/LobbyMenu.js'
 export default function Game({ navigation }) {
     return (
 
-<<<<<<< HEAD
+
         <View>
             <LobbyMenu />
         </View>
-=======
-            <View><Text>This is the game screen</Text></View>
-            <Map />
->>>>>>> 99e41c53d90ed70210e0b214e23693a462e85172
+
+
 
 
     );
